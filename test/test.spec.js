f769fc16b69c50ba6a26f0d2e0f07593e923d0bc
Clay Carpenter <claycarpenter@gmail.com>
2016-06-25 16:57:45 -0400

    Gets Karma working with a "real" Angular test.
    Adds Babel polyfill dependency and includes the polyfill in the Karma files list.
    
    Adds waiting-button module entry script to Karma files list.
    
    Updates Karma to print debug output.

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,10 +1,34 @@
 import {testFunction} from './test.import';
+const angular = require('angular'),
+      angularMock = require('angular-mocks');
 
 
 describe('Test component', () => {
+  beforeEach(() => {
+    angular.mock.module('mbm.waitingButton');
+  });
+
   it('has a dummy test', () => {
     const result = testFunction();
 
     expect(result).toEqual(42);
+  });
+
+  it('should compile this directive', () => {
+    angular.mock.inject(($rootScope, $compile, $timeout) => {
+      $rootScope.onClickReceiver = function() {
+        return new Promise((resolve, reject) => {
+          resolve(true);
+        });
+        // return $timeout(() => {}, 0, false);
+      }
+      const template = `
+        <button mbm-waiting-button="onClickReceiver()">Waiting Button</button>
+      `;
+      const element = $compile(template)($rootScope);
+
+      console.log(`element:`, element[0]);
+      element[0].click();
+    });
   });
 });
