0805be5a60cd728b28ee958b1bc7b2566a2fdcae
Clay Carpenter <claycarpenter@gmail.com>
2016-06-26 10:43:00 -0400

    Adds test for sequential success operations.

--- test/waiting-button.directive.spec.js
+++ test/waiting-button.directive.spec.js
@@ -174,4 +174,56 @@ describe('mbmWaitingButton', () => {
       expect(element.hasClass(WaitingButtonController.WAITING_CLASS)).toEqual(false);
     });
   });
+
+  it('should support sequential success operations', () => {
+    angular.mock.inject(($rootScope, $compile, $timeout, $q) => {
+      // Build a Promise to represent an async operation
+      let operationDeferred = $q.defer();
+
+      // Fake receiver for the click action
+      $rootScope.onClickReceiver = function() {
+        return operationDeferred.promise;
+      }
+
+      // Define simple template for the directive, and then compile it
+      const template = `
+        <button mbm-waiting-button="onClickReceiver()">Waiting Button</button>
+      `;
+      const element = $compile(template)($rootScope);
+
+      // Run the click test once
+      successClickTest();
+
+      // Reset the operation promise
+      operationDeferred = $q.defer();
+
+      // Run the click test again
+      successClickTest();
+
+      function successClickTest() {
+        // Verify that the element doesn't have the waiting-button loading class
+        //  by default.
+        expect(element.hasClass(WaitingButtonController.WAITING_CLASS)).toEqual(false);
+
+        // Simulate click to enter "waiting" state
+        element[0].click();
+
+        // Run the Angular digest loop once to pick up the changes.
+        $rootScope.$digest();
+
+        // Verify that the element now has the waiting-button loading class.
+        expect(element.hasClass(WaitingButtonController.WAITING_CLASS)).toEqual(true);
+
+        // Resolve the operation. Should return waiting button to normal state.
+        operationDeferred.resolve();
+
+        // Run the Angular digest loop once to pick up the changes.
+        $rootScope.$digest();
+
+        // Verify that the element doesn't have the waiting-button loading class
+        //  after the operation promise has resolved.
+        expect(element.hasClass(WaitingButtonController.WAITING_CLASS)).toEqual(false);
+      }
+    });
+  });
 });
