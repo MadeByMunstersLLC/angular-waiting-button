0acc9b7c03940d0be3a5225d62eeff36cfd7b7f8
Clay Carpenter <claycarpenter@gmail.com>
2016-06-26 09:47:31 -0400

    Adds test for custom waiting class.

--- test/waiting-button.directive.spec.js
+++ test/waiting-button.directive.spec.js
@@ -13,9 +13,6 @@ describe('mbmWaitingButton', () => {
       // Build a Promise to represent an async operation
       const operationDeferred = $q.defer();
 
-      // TODO: Maybe should use $q here?
-      const operationPromise = new Promise((resolve, reject) => {});
-
       // Fake receiver for the click action
       $rootScope.onClickReceiver = function() {
         return operationDeferred.promise;
@@ -40,9 +37,6 @@ describe('mbmWaitingButton', () => {
       // Verify that the element now has the waiting-button loading class.
       expect(element.hasClass(WaitingButtonController.WAITING_CLASS)).toEqual(true);
 
-      // TODO: this functionality has yet to be implemented.
-      // expect(element.prop('disabled')).toEqual(true);
-
       // Resolve the operation. Should return waiting button to normal state.
       operationDeferred.resolve();
 
@@ -52,6 +46,53 @@ describe('mbmWaitingButton', () => {
       // Verify that the element doesn't have the waiting-button loading class
       //  after the operation promise has resolved.
       expect(element.hasClass(WaitingButtonController.WAITING_CLASS)).toEqual(false);
+    });
+  });
+
+  it('should support customizable waiting classes', () => {
+    angular.mock.inject(($rootScope, $compile, $timeout, $q) => {
+      // Build a Promise to represent an async operation
+      const operationDeferred = $q.defer();
+
+      // Fake receiver for the click action
+      $rootScope.onClickReceiver = function() {
+        return operationDeferred.promise;
+      }
+
+      // Define the custom CSS class we'll be testing for
+      const waitingClass = 'waiting';
+
+      // Define simple template for the directive, and then compile it
+      const template = `
+        <button
+          mbm-waiting-button="onClickReceiver()"
+          waiting-button-waiting-class="${waitingClass}"
+          >Waiting Button</button>
+      `;
+      const element = $compile(template)($rootScope);
+
+      // Verify that the element doesn't have the waiting-button loading class
+      //  by default.
+      expect(element.hasClass(waitingClass)).toEqual(false);
+
+      // Simulate click to enter "waiting" state
+      element[0].click();
+
+      // Run the Angular digest loop once to pick up the changes.
+      $rootScope.$digest();
+
+      // Verify that the element now has the waiting-button loading class.
+      expect(element.hasClass(waitingClass)).toEqual(true);
+
+      // Resolve the operation. Should return waiting button to normal state.
+      operationDeferred.resolve();
+
+      // Run the Angular digest loop once to pick up the changes.
+      $rootScope.$digest();
+
+      // Verify that the element doesn't have the waiting-button loading class
+      //  after the operation promise has resolved.
+      expect(element.hasClass(waitingClass)).toEqual(false);
     });
   });
 });
