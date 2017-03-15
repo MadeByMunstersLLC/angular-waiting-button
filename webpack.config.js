75168f0dedc3c165507fbe83ac2dec46352395e4
Josh Hamilton <nearbycoder@gmail.com>
2017-03-15 13:48:45 -0500

    Add, update, and remove configs to change out Gulp for Webpack.

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require("webpack");
+var path = require("path");
+
+var BUILD_DIR = path.resolve(__dirname, "dist");
+var APP_DIR = path.resolve(__dirname, "./src/js");
+var config = {
+  devtool: "source-map",
+  entry: APP_DIR + "/waiting-button.module.js",
+  output: {
+    path: BUILD_DIR,
+    filename: "angular-waiting-button.js",
+    library: "mbmWaitingButton",
+    libraryTarget: "commonjs2"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ["babel-loader"],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      angular: path.resolve(path.join(__dirname, "node_modules", "angular"))
+    }
+  }
+};
+
+module.exports = config;
